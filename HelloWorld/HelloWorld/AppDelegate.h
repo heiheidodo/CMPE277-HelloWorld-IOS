ef90797a23aca32f9096487afc4a8785d47dca21
Sheng Zhou <heiheidodo@gmail.com>
2017-05-01 15:16:36 -0700

    Upload files

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by ZHOUSHENG on 4/17/17.
+//  Copyright © 2017 ZHOUSHENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
